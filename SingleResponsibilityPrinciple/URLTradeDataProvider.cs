121530ab405c57ff4d6407c6394df72fa2667d9a
Mikita Puntus <150572042+mpuntus-css@users.noreply.github.com>
2024-12-20 04:27:58 -0600

    Part 1: Test Driven Design (TDD) for trades from URL
    Passed the Unit Test.
    
    Request: You decide not to use a Stream for input but instead read the trades from a remote call to a web service
    
    Request 407 - "As a trader, I want to be able to able to read trades from the companies new web service provider so I can enter trades from different apps."
    TDD: Get the URLTradeDataProviderTests to pass
    Details: You must modify the URLTradeDataProvider class and make it work, reading a file from the url passed in as a parameter to the constructor.
    Solution: Modify the GetTradeData() method that supplies the data from the service.
    Make sure Program.cs and URLTradeDataProviderTests is using the updated trade URL for Fall 2024:
    string tradeURL = "http://raw.githubusercontent.com/tgibbons-css/CIS3285_Unit9_F24/refs/heads/master/SingleResponsibilityPrinciple/trades.txt";
    Here is some starting code to read from a URL.
        List<string> tradeData = new List<string>();
        logger.LogInfo("Reading trades from URL: " + url);
    
        using (HttpClient client = new HttpClient())
        {
            HttpResponseMessage response = client.GetAsync(url).Result;
            if (!response.IsSuccessStatusCode)
            {
                // log error and throw an exception if the URL fails
            }
            // set up a Stream and StreamReader to access the data
            using (Stream stream = response.Content.ReadAsStreamAsync().Result)
            using (StreamReader reader = new StreamReader(stream))
            {
                // Read each line of the text file using reader.ReadLine()
                // Read until the reader returns a null line
                // Add each line to the tradeData list
            }
        }

--- SingleResponsibilityPrinciple/URLTradeDataProvider.cs
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,56 @@ namespace SingleResponsibilityPrinciple
 {
     public class URLTradeDataProvider : ITradeDataProvider
     {
-        public URLTradeDataProvider(string url, ILogger logger)
-        {
+        string url;
+        ILogger logger;
+        
+            public URLTradeDataProvider(string url, ILogger logger)
+            {
+                this.url = url;
+                this.logger = logger;
+            }
+
+            public IEnumerable<string> GetTradeData()
+            {
+            List<string> tradeData = new List<string>();
+            logger.LogInfo("Reading trades from URL: " + url);
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    // log error and throw an exception if the URL fails
+                    logger.LogWarning($"Failed to retrieve data. Status code: {response.StatusCode}");
+                    throw new Exception($"Error retrieving data from URL: {url}");
+
+                }
+                // set up a Stream and StreamReader to access the data
+                using (Stream stream = response.Content.ReadAsStreamAsync().Result)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    // Read each line of the text file using reader.ReadLine()
+                    // Read until the reader returns a null line
+                    // Add each line to the tradeData list
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        tradeData.Add(line);
+                    }
+
+                }
+            }
+            return tradeData;
+
         }
 
-        public IEnumerable<string> GetTradeData()
-        {
-            throw new NotImplementedException();
-        }
+
+
+
+
+
+
+
+
     }
 }
